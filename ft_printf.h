ed5b31ccec7f3114d9a098427d620c205317e52b
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-22 14:22:27 +0200

    .h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 09:24:44 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/16 12:01:50 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/06/22 14:07:12 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ int	ft_putchar(char c);
 int	ft_putnbr(int n);
 int	ft_putnbr_unsigned(unsigned int n);
 int	ft_putnbr_base(unsigned int nb, int pos);
-int	ft_putnbr_long_base(long nb);
+int	ft_putnbr_long_base(void *nb);
 
 #endif
