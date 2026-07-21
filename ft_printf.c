060467e403ddf3d16ba1b9328951da5e92663378
Ptitchat <pau.roy.narvaez@gmail.com>
2026-07-21 16:26:09 +0200

    changed count

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 08:23:12 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/22 14:04:16 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/07/21 15:36:31 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ static int	ft_print_vargs(char const *format, va_list vargs)
 		count_printed += ft_putnbr_base(va_arg(vargs, unsigned int), 1);
 	else if (*format == '%')
 		count_printed += ft_putchar('%');
-	else
-		count_printed--;
+	/*else
+		count_printed--;*/
 	return (count_printed);
 }
 
@@ -52,7 +52,7 @@ int	ft_choose_format(char const *format, va_list vargs)
 		if (*format == '%' && *(format + 1))
 		{
 			count_printed += ft_print_vargs(format + 1, vargs);
-			if (count_printed < temp)
+			if (count_printed == temp)
 				count_printed += ft_putchar((char) *format);
 			else
 				format++;
