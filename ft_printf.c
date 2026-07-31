992c81cfef71ac88b909ac2ab571c1979bca4c9a
Ptitchat <pau.roy.narvaez@gmail.com>
2026-07-31 14:58:55 +0200

    control empty flag %s

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 08:23:12 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/07/31 09:41:51 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/07/31 14:52:41 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ static int	ft_print_vargs(char const *format, va_list vargs)
 		count_printed += ft_putstr(va_arg(vargs, char *));
 	else if (*format == 'p')
 		count_printed += ft_putnbr_long_base(va_arg(vargs, void *));
-	else if (*format == 'd')
-		count_printed += ft_putnbr(va_arg(vargs, int));
-	else if (*format == 'i')
+	else if (*format == 'd' || *format == 'i')
 		count_printed += ft_putnbr(va_arg(vargs, int));
 	else if (*format == 'u')
 		count_printed += ft_putnbr_unsigned(va_arg(vargs, unsigned int));
@@ -52,13 +50,15 @@ int	ft_choose_format(char const *format, va_list vargs)
 			if (*(format + 1) == '\0')
 				return (-1);
 			count_printed += ft_print_vargs(format + 1, vargs);
-			if (count_printed == temp)
+			if (count_printed == temp && *(format + 1) != 's')
 				count_printed += ft_putchar((char) *format);
 			else
 				format++;
 		}
 		else
 			count_printed += ft_putchar((char) *format);
+		if (count_printed < temp)
+			return (-1);
 		format++;
 	}
 	return (count_printed);
@@ -70,8 +70,6 @@ int	ft_printf(char const *format, ...)
 	int			count_printed;
 
 	if (!format)
-		return (-1);
-	if (write(1, "", 1) < 0)
 		return (-1);
 	va_start(vargs, format);
 	count_printed = ft_choose_format(format, vargs);
